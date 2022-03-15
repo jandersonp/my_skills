45d8f9c84bedcc4b85032bcbab85337a825d0746
jandersonp <jandersonpizziallyvilasboas@gmail.com>
2022-03-15 13:22:12 -0300

    feat: create component home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,64 @@
 import React from 'react'
 
-import { Text } from 'react-native'
+import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native'
 
 function Home() {
   return (
-    <Text>Home</Text>
+    <>
+      <View style={styles.container}>
+        <Text style={styles.title}>Bem-vindo, Janderson!</Text>
+
+        <TextInput
+          style={styles.input}
+          placeholder="Nova Skill"
+          placeholderTextColor="#555"
+        />
+        <TouchableOpacity
+          style={styles.button}
+          activeOpacity={0.7}
+        >
+          <Text style={styles.buttonText}>Adicionar</Text>
+        </TouchableOpacity>
+        
+        <Text style={[styles.buttonText, { marginTop: 50}]}>Minhas Skills:</Text>
+      </View>
+    </>
   )
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121015',
+    paddingVertical: 70,
+    paddingHorizontal: 30
+  },
+  title: {
+    color: '#FFF',
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  input: {
+    backgroundColor: '#1F1E25',
+    color: '#FFF',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 18,
+    marginTop: 30,
+    borderRadius: 7
+  },
+  button: {
+    backgroundColor: '#A370F7',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 17,
+    fontWeight: 'bold',
+  }
+})
 
 export { Home }
